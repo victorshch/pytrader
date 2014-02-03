722b573760123fd4b4d0675f693e803622577cd1
Victor Shcherbinin <victorshch@gmail.com>
2014-02-03 11:34:47 +0400

    traderthread: now uses arbmath module
    Signed-off-by: Victor Shcherbinin <victorshch@gmail.com>

--- traderthread.py
+++ traderthread.py
@@ -1,10 +1,12 @@
 import sys
 import decimal
 from decimal import Decimal
+import copy
 import PyQt4
 from PyQt4 import QtCore
 from PyQt4.QtCore import pyqtSlot
 from PyQt4.QtCore import pyqtSignal
+import arbmath
 
 class Tops(object):
   ask1 = Decimal('0')
@@ -90,150 +92,66 @@ class TraderThread(QtCore.QThread):
       profit1 = k3 * (b3) / ((a1) * (a2)) - Decimal('1.0')
       profit2 = k3 * (b1) * (b2) / (a3)  - Decimal('1.0')
       
-      if profit1 > 0:
-        self.tradeForward()
-      elif profit2 > 0:
-        self.tradeBackward()
+      balance = { self.s1: min(self.balance[self.s1], self.s1ToSpend), \
+        self.s2: min(self.balance[self.s2], self.s2ToSpend), \
+        self.s3: min(self.balance[self.s3], self.s3ToSpend) }
       
-      a1 = ArbData("Forward", profit1)
-      a2 = ArbData("Backward", profit2)
+      orders, maxDepth = arbmath.CalculateArbOrders(self.depths, self.p1, self.p2, self.p3, self.k, balance, self.tradeAPI)
+      
+      usdProfit = Decimal('0.0')
+      
+      if orders:
+        print "Exploring arbitrage opportunity: %s" % orders
+        
+        exchangeModel = arbmath.ExchangeModel(self.depths, self.tradeAPI)
+        
+        newBalance = copy.deepcopy(balance)
+        for order in orders:
+          newBalance, pendingOrder = exchangeModel.ModelTrade(newBalance, *order)
+        
+        usdProfit = newBalance[self.s1] - balance[self.s1]
+        
+        print "Profit: %s" % usdProfit
+        
+        s2Change = newBalance[self.s2] - balance[self.s2]
+        if s2Change > 0:
+          usdProfit += s2Change * b1
+        else:
+          usdProfit += s2Change * a1
+          
+        s3Change = newBalance[self.s3] - balance[self.s3]
+        if s3Change > 0:
+          usdProfit += s3Change * b3
+        else:
+          usdProfit += s3Change * a3
+        
+        print "%s gain: %s" % (self.s2, s2Change)
+        print "%s gain: %s" % (self.s3, s3Change)
+        
+        print "Overall profit: %s" % usdProfit
+        
+        if not self.tradeTimer.isActive():
+          if usdProfit > self.minProfit:
+            for order in orders:
+              tradeAPI.EnqueueOrder(*order)
+            
+            print "Placing orders..."
+            r1, r2, r3 = tradeAPI.ProcessPendingOrders()
+            
+            print "Received from trade 1: %s" % r1
+            print "Received from trade 2: %s" % r2
+            print "Received from trade 3: %s" % r3
+            
+            self.tradeTimer.start()
+          else:
+            print "Profit %s less than min profit %s" % (usdProfit, self.minProfit)
+        else:
+          print "Trade timer is active, not trading"
+          
+      a1 = ArbData("Forward", profit1, usdProfit)
+      a2 = ArbData("Backward", profit2, usdProfit)
 
       self.updateData.emit(t, a1, a2)
-
-  def tradeForward(self):
-    a1, X, b1, X2 = self.tops.ask1, self.tops.ask1_amount, self.tops.bid1, self.tops.bid1_amount
-    a2, Y, b2, Y2 = self.tops.ask2, self.tops.ask2_amount, self.tops.bid2, self.tops.bid2_amount
-    a3, Z2, b3, Z = self.tops.ask3, self.tops.ask3_amount, self.tops.bid3, self.tops.bid3_amount
-    
-    print "%s: exploring %s->%s->%s->%s arbitrage opportunity" % (str(QtCore.QDateTime.currentDateTime().toString()), self.s1, self.s2, self.s3, self.s1)
-    
-    k = self.k
-    k2 = self.k * self.k
-    k3 = k2 * self.k
-    
-    s1 = self.s1
-    s2 = self.s2
-    s3 = self.s3
-    p1 = self.p1
-    p2 = self.p2
-    p3 = self.p3
-    
-    usdToSpend = min(self.balance[s1], self.s1ToSpend, a1 / k * min(X, self.s2ToSpend, self.balance[s2]), a1 * a2 / k2 * min(Y, self.s3ToSpend, self.balance[s3]), a1 * a2 / k3 * Z)
-    
-    profit1 = k3 * b3 / (a1 * a2) - Decimal('1.0')
-    
-    usdProfit = usdToSpend * profit1
-    if(usdProfit < self.minProfit):
-      print "Investment " + str(usdToSpend) + ", profit " + str(usdProfit) + " less than " + str(self.minProfit)
-      return False
-      
-    btcToBuy = self.tradeAPI.FormatAmount(p1, k * usdToSpend / a1)
-    ltcToBuy = self.tradeAPI.FormatAmount(p2, k * btcToBuy / a2)
-    
-    if btcToBuy < self.tradeAPI.GetMinAmount(p1):
-      print "%s to buy %s less than minimum %s" %(s2, btcToBuy, self.tradeAPI.GetMinAmount(p1))
-      return False
-      
-    if ltcToBuy < min(self.tradeAPI.GetMinAmount(p2), self.tradeAPI.GetMinAmount(p3)):
-      print "%s to buy %s less than minimum %s" %(s3, ltcToBuy, min(self.tradeAPI.GetMinAmount(p2), self.tradeAPI.GetMinAmount(p3)))
-      return False
-    
-    if self.lag > self.maxLag:
-      print "Lag %s is greater than maximum lag %s, not trading" % (self.lag, self.maxLag)
-      return False
-    
-    if not self.tradeTimer.isActive():
-      print "Buying %s %s at price %s %s" % (btcToBuy, s2, a1, s1)
-      
-      self.tradeAPI.EnqueueOrder(p1, "buy", a1, btcToBuy)
-      
-      print "Buying %s %s at price %s %s" % (ltcToBuy, s3, a2, s2)
-      
-      self.tradeAPI.EnqueueOrder(p2, "buy", a2, ltcToBuy)
-      
-      print "Selling %s %s at price %s %s" % (ltcToBuy, s3, b3, s1)
-      
-      self.tradeAPI.EnqueueOrder(p3, "sell", b3, ltcToBuy)
-      
-      print "Executing orders..."
-      
-      result = self.tradeAPI.PlacePendingOrders()
-      
-      print "Trade 1, received: " + str(result[0].received)
-      print "Trade 2, received: " + str(result[1].received)
-      print "Trade 3, received: " + str(result[2].received)
-      
-      tradeTimer.start()
-      return True
-    else:
-      print "Trade timer is active, not trading"
-      
-      return False
-  def tradeBackward(self):
-    a1, X2, b1, X = self.tops.ask1, self.tops.ask1_amount, self.tops.bid1, self.tops.bid1_amount
-    a2, Y2, b2, Y = self.tops.ask2, self.tops.ask2_amount, self.tops.bid2, self.tops.bid2_amount
-    a3, Z, b3, Z2 = self.tops.ask3, self.tops.ask3_amount, self.tops.bid3, self.tops.bid3_amount
-    
-    print "%s: exploring %s->%s->%s->%s arbitrage opportunity" % (str(QtCore.QDateTime.currentDateTime().toString()), self.s1, self.s3, self.s2, self.s1)
-    
-    k = self.k
-    k2 = self.k * self.k
-    k3 = k2 * self.k
-    
-    s1 = self.s1
-    s2 = self.s2
-    s3 = self.s3
-    p1 = self.p1
-    p2 = self.p2
-    p3 = self.p3
-    
-    usdToSpend = min(self.s1ToSpend, self.balance[s1], a3 / k * min(Z, self.s3ToSpend, self.balance[s3]), a3 / (k2 * b2) * min(Y * b2, self.s2ToSpend, self.balance[s2]), X * a3 / (k3 * b2))
-    
-    profit2 = k3 * b1 * b2 / a3 - Decimal('1.0')
-
-    usdProfit = usdToSpend * profit2
-    if(usdProfit < self.minProfit):
-      print "Investment " + str(usdToSpend) + ", profit " + str(usdProfit) + " less than " + str(self.minProfit)
-      return False
-      
-    ltcToBuy = self.tradeAPI.FormatAmount(p3, k * usdToSpend / a3)
-    btcToBuy = self.tradeAPI.FormatAmount(p1, k * ltcToBuy * b2)
-    ltcToBuy = self.tradeAPI.FormatAmount(p3, btcToBuy / (k * b2))
-    usdToGain = k * btcToBuy * b1
-    
-    if btcToBuy < self.tradeAPI.GetMinAmount(p1):
-      print "%s to buy %s less than minimum %s" %(s2, btcToBuy, self.tradeAPI.GetMinAmount(p1))
-      return False
-      
-    if ltcToBuy < min(self.tradeAPI.GetMinAmount(p2), self.tradeAPI.GetMinAmount(p3)):
-      print "%s to buy %s less than minimum %s" %(s3, ltcToBuy, min(self.tradeAPI.GetMinAmount(p2), self.tradeAPI.GetMinAmount(p3)))
-      return False
-      
-    if self.lag > self.maxLag:
-      print "Lag %s is greater than maximum lag %s, not trading" % (self.lag, self.maxLag)
-      return False
-    
-    if not self.tradeTimer.isActive():
-      print "Buying " + str(ltcToBuy) + " ltc at price " + str(a3) + " usd"
-      
-      self.tradeAPI.EnqueueOrder(p3, "buy", a3, ltcToBuy)
-      
-      print "Selling " + str(ltcToBuy) + " ltc at price " + str(b2) + " btc"
-      
-      self.tradeAPI.EnqueueOrder(p2, "sell", b2, ltcToBuy)
-      
-      print "Selling " + str(btcToBuy) + " btc at price " + str(b1) + " usd"
-      
-      self.tradeAPI.EnqueueOrder(p1, "sell", b1, btcToBuy)
-      
-      print "Executing orders..."
-      result1, result2, result3 = self.tradeAPI.PlacePendingOrders()
-      
-      print "Trade 1, received: " + str(result1.received)
-      print "Trade 2, received: " + str(result2.received)
-      print "Trade 3, received: " + str(result3.received)
-    else:
-      print "Trade timer is active, not trading"
 
   def run(self):
     self.timer.start()
@@ -245,12 +163,14 @@ class TraderThread(QtCore.QThread):
   
   def GetTops(self):
     result = Tops()
-    depthsAndBalance  = self.tradeAPI.GetDepths([self.p1, self.p2, self.p3], 1, 1, 5)
+    depthsAndBalance  = self.tradeAPI.GetDepths([self.p1, self.p2, self.p3], 50, 50, 5)
     r = depthsAndBalance['depths']
     self.balance = depthsAndBalance['balance']
     p1Top = ((r[self.p1]['ask'][0]['price'], r[self.p1]['ask'][0]['amount']), (r[self.p1]['bid'][0]['price'], r[self.p1]['bid'][0]['amount']))
     p2Top = ((r[self.p2]['ask'][0]['price'], r[self.p2]['ask'][0]['amount']), (r[self.p2]['bid'][0]['price'], r[self.p2]['bid'][0]['amount']))
     p3Top = ((r[self.p3]['ask'][0]['price'], r[self.p3]['ask'][0]['amount']), (r[self.p3]['bid'][0]['price'], r[self.p3]['bid'][0]['amount']))
+    
+    self.depths = r
     
     result.ask1 = p1Top[0][0]
     result.ask1_amount = p1Top[0][1]
